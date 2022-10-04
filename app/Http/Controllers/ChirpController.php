7e3addcd296deccc24b18bb96fda2cdc2de6c735
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-10-04 18:56:13 +0900

    wip

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
